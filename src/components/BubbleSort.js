a29659edea26cdc1f898dc2972f1cb7902bc9825
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-08-04 19:19:23 -0700

    still no idea but it works now

--- src/components/BubbleSort.js
+++ src/components/BubbleSort.js
@@ -4,26 +4,26 @@ import SortStep from './SortStep'
 const BubbleSort = (props) => {
   const [steps, setSteps] = useState([])
   const [sortedData, setSortedData] = useState([])
-  const [refresh, setRefresh] = [0]
+  const [refresh, setRefresh] = useState(0)
 
   const bubbleSortArray = arr => {
     console.log(arr)
-    var sorted = new Array(...arr)
-    console.log(sorted)
-    var steps = []
-    console.log(sorted)
-    // console.log(steps)
-    for(let i=0; i<sorted.length; i++){
-      for(let j=0; j<sorted.length-i-1; j++){
-        steps.push(sorted)
-        if(sorted[j] > sorted[j + 1]){
-          [sorted[j], sorted[j + 1]] = [sorted[j +1], sorted[j]]
+    const initialArray = new Array(...arr)
+    const sortingSteps = []
+
+    for (let i = 0; i < initialArray.length; i++) {
+      for (let j = 0; j < initialArray.length - i - 1; j++) {
+        sortingSteps.push([...initialArray])
+        // console.log(sortingSteps)
+        if (initialArray[j] > initialArray[j + 1]) {
+          [initialArray[j], initialArray[j + 1]] = [initialArray[j + 1], initialArray[j]]
         }
       }
     }
-    setSteps(steps)
-    console.log(steps)
-    return sorted
+
+    setSteps(sortingSteps)
+
+    return initialArray
   }
 
   // const sortedArray = bubbleSortArray(props.data)
@@ -54,15 +54,15 @@ const BubbleSort = (props) => {
         So our steps for processing data looks like this:
       </li>
       <li id="processedData">
-        {steps.map((step, index) => {
-          return(
-            <SortStep key={index} data={step}/>
+        {/* {steps.map((step, index) => {
+          return (
+            <SortStep key={index} data={step} />
           )
-        })}
+        })} */}
       </li>
       <li>
         Our fully sorted data:
-        <SortStep data={sortedData}/>
+        {/* <SortStep data={sortedData} /> */}
       </li>
     </>
   )
